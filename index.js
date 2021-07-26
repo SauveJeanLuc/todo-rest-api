034f767692bddaaa6039e0354275ba7a7ee26922
SAUVE Jean-Luc <sauvejeanluc3@gmail.com>
2021-07-26 08:23:02 +0200

    mongoose included

---
+++ index.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const Joi = require('joi');
+const items = require('./routes/items');
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+app.use('/api/genres', genres);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
